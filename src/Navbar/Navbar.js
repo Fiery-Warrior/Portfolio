fd581df5220c7497ef0dc4d5ce13980923ca070b
Keagan <keagan.j.bogart@gmail.com>
2023-05-23 00:01:43 -0500

    Inital navbar

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,60 @@
+import React from "react";
+import './nav.css';
+const Navbar = () => {
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    element.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">
+      <li className="nav-item">
+          <a
+            className="nav-link"
+          >
+          <a className="nav-link" dangerouslySetInnerHTML={{ __html: "&lt;JohnJames/&gt;" }}></a>
+          </a>
+        </li>        
+        <li className="nav-item">
+          <a
+            href="#about"
+            className="nav-link"
+            onClick={() => scrollToSection("about")}
+          >
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#experience"
+            className="nav-link"
+            onClick={() => scrollToSection("experience")}
+          >
+            Experience
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#contact"
+            className="nav-link"
+            onClick={() => scrollToSection("contact")}
+          >
+            Contact
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#resume"
+            className="nav-link"
+            onClick={() => scrollToSection("resume")}
+          >
+            Resume
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
