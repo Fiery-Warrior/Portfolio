b821f257a3e6d819a369d9ba10c5993cec7f4d2f
Keagan <keagan.j.bogart@gmail.com>
2023-05-25 00:37:31 -0500

    navbar and about sections are complete but still need to set spesfic adjustment for phone sizes

--- src/About/image.jsx
+++ src/About/image.jsx
@@ -1,30 +1,83 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
-import { RiMailFill } from 'react-icons/ri';
+import React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
+import { RiMailFill } from "react-icons/ri";
 
 export default function ImgMediaCard() {
   return (
-    <Card sx={{ maxWidth: 345, minWidth: 440 }}>
+    <Card
+      sx={{
+        maxWidth: 345,
+        minWidth: 440,
+        "@media (max-width: 768px)": {
+          maxWidth: 300,
+          minWidth: "auto",
+        },
+        "@media (max-width: 480px)": {
+          maxWidth: 250,
+        },
+      }}
+    >
       <CardMedia
         component="img"
         alt="Beach Walk"
         height="400"
         image="\me.jpg"
+        sx={{
+          "@media (max-width: 768px)": {
+            height: 300,
+          },
+          "@media (max-width: 480px)": {
+            height: 250,
+          },
+        }}
       />
       <CardActions>
-          <a href="https://www.linkedin.com/in/keagan-bogart-786951209" target="_blank">
-            <FaLinkedin size={25} style={{ color: "#0A66C2", paddingRight: "3%"  }}/>
-          </a>
-          <a href="https://github.com/Fiery-Warrior/mm_pythonweb" target="_blank">
-            <FaGithubSquare size={25} style={{ color: "#000000", paddingRight: "3%" }}/>
-          </a>
-          <a href="mailto:keagan.j.bogart@gmail.com">
-            <RiMailFill size={25} style={{ color: "#D44638", paddingRight: "3%" }}/>
-          </a>
+        <a
+          href="https://www.linkedin.com/in/keagan-bogart-786951209"
+          target="_blank"
+        >
+          <FaLinkedin
+            size={25}
+            style={{
+              color: "#0A66C2",
+              paddingRight: "3%",
+              "@media (max-width: 480px)": {
+                fontSize: 20,
+              },
+            }}
+          />
+        </a>
+        <a
+          href="https://github.com/Fiery-Warrior/mm_pythonweb"
+          target="_blank"
+        >
+          <FaGithubSquare
+            size={25}
+            style={{
+              color: "#000000",
+              paddingRight: "3%",
+              "@media (max-width: 480px)": {
+                fontSize: 20,
+              },
+            }}
+          />
+        </a>
+        <a href="mailto:keagan.j.bogart@gmail.com">
+          <RiMailFill
+            size={25}
+            style={{
+              color: "#D44638",
+              paddingRight: "3%",
+              "@media (max-width: 480px)": {
+                fontSize: 20,
+              },
+            }}
+          />
+        </a>
       </CardActions>
     </Card>
   );
