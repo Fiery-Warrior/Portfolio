d3d5cc74dfc2699176b98128aac81901d52fd01d
Keagan <keagan.j.bogart@gmail.com>
2023-07-01 17:15:07 -0500

    changes2

---
+++ src/Landing/Landing.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "./landing.css";
+import Typed from "react-typed";
+
+const Landing = () => {
+  return (
+    <section id="home"
+      style={{
+        position: "absolute",
+        top: 0,
+        left: "20%",
+        height: "100vh",
+        width: "79vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundImage: "url('/me8.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      
+      <div style={{ position: "absolute", top: "82%", left: "65%", transform: "translateX(-50%)" }}>
+        <h1 style={{ textAlign: "center", color: "#fff", fontSize: "37px" }}></h1> {/**Keagan Bogart */}
+        <h3 style={{ textAlign: "center", color: "#fff", marginBottom: "0.5rem",fontSize: "35px", }}>
+          I'm a {" "}
+          <Typed
+            strings={[
+              "Full Stack Developer",
+              "Cloud Engineer",
+              "Creator",
+              "Dreamer",
+            ]}
+            typeSpeed={120}
+            backSpeed={140}
+            loop
+            style={{ color: "#fff", borderBottom: "3px solid #149ddd",}}
+          />
+        </h3>
+        <section
+          sx={{
+            maxWidth: 345,
+            minWidth: 440,
+            "@media (max-width: 768px)": {
+              maxWidth: 300,
+              minWidth: "auto",
+            },
+            "@media (max-width: 480px)": {
+              maxWidth: 250,
+            },
+          }}
+        >
+
+        </section>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
