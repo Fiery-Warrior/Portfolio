bce2250463c52a5f8e692222c6908a1470f061ed
Fiery-Warrior <keagan.j.bogart@gmail.com>
2023-07-01 13:26:15 -0500

    Typing and landing page

--- src/About/About.js
+++ src/About/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./about.css";
-import Card from "@mui/material/Card";
+import Typed from "react-typed";
 
 const About = () => {
   return (
@@ -9,32 +9,49 @@ const About = () => {
         position: "absolute",
         top: 0,
         left: "20%",
-        height: "150vh",
+        height: "100vh",
         width: "80vw",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        backgroundImage: "url('/me2.jpg')",
+        backgroundImage: "url('/me8.jpg')",
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
     >
-      <Card
-        sx={{
-          maxWidth: 345,
-          minWidth: 440,
-          "@media (max-width: 768px)": {
-            maxWidth: 300,
-            minWidth: "auto",
-          },
-          "@media (max-width: 480px)": {
-            maxWidth: 250,
-          },
-        }}
-      >
-  
-        
-      </Card>
+      <div style={{ position: "absolute", top: "70%", left: "60%", transform: "translateX(-50%)" }}>
+        <h1 style={{ textAlign: "center", color: "#fff", fontSize: "40px" }}>Keagan Bogart</h1>
+        <h3 style={{ textAlign: "center", color: "#fff", marginBottom: "1rem" }}>
+          I'm a {" "}
+          <Typed
+            strings={[
+              "Full Stack Developer",
+              "Cloud Engineer",
+              "Creator",
+              "Dreamer",
+            ]}
+            typeSpeed={150}
+            backSpeed={100}
+            loop
+            style={{ color: "#fff", borderBottom: "3px solid #149ddd",}}
+          />
+        </h3>
+        <section
+          sx={{
+            maxWidth: 345,
+            minWidth: 440,
+            "@media (max-width: 768px)": {
+              maxWidth: 300,
+              minWidth: "auto",
+            },
+            "@media (max-width: 480px)": {
+              maxWidth: 250,
+            },
+          }}
+        >
+
+        </section>
+      </div>
     </div>
   );
 };
