4da48b56390d7b479a65bd6a4fd60fa4fae71517
Keagan <keagan.j.bogart@gmail.com>
2023-07-30 17:08:32 -0500

    updated text

--- src/About/About.js
+++ src/About/About.js
@@ -11,13 +11,13 @@ const About = () => {
           {/* <h1>Who am I?</h1> */}
           <h1 style={{ textAlign: "left" }}>Who am I?</h1>
           <p style={{ textAlign: "left" }}>
-            Hello! I'm Keagan Bogart, a Senior Cyber Security Student at DBU.
+            Hello! I'm Keagan Bogart, a Senior Cyber Security Student at Dallas Baptist University majoring in Cybersecurity, graduating December 2023.
             <br />
             <br />
             Currently, I'm working as a Teaching Assistant for the Cyber Security and Computer Science department, where I lead labs, grade assignments, tutor students, and manage other TAs.
             <br />
             <br />
-            These past two years I've had the opportunity to work as a Full Stack Software Engineer at USAA. I am returning for a second internship where I will also be working as a Cloud Developer.
+            These past two years I've had the opportunity to work as a Full Stack Software Engineer at USAA. I was a Full Stack Software Engineer for the first internship and I am returning for a second internship as a Cloud Engineer.
           </p>
     </section>
         </div>
