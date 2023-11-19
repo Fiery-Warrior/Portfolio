a6ca31cf69c396d6f9595d555e93b5af43333d8f
Keagan Bogart <keagan.j.bogart@gmail.com>
2023-11-18 18:12:24 -0600

    Made it so when a side nav element is hovered over the others all blur out and that one pops out

--- src/navi.jsx
+++ src/navi.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "react-bootstrap/Image";
 import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
 import { RiMailFill } from "react-icons/ri";
@@ -8,23 +8,48 @@ import { IoIosPaper } from "react-icons/io";
 import { GiTeacher, GiRotaryPhone } from "react-icons/gi";
 import "./navi.css";
 
+const styles = {
+  link: {
+    color: "#ccc",
+    margin: "5px 0",
+    transition: "all 0.3s ease",
+    textDecoration: "none", // Remove underline
+    fontSize: "85%",
+  },
+hover: {
+  transform: "scale(1.1)", // Make the link "pop up"
+  zIndex: 1, // Make sure the link is above the others
+},
+blur: {
+  filter: "blur(2px)", // Blur the other links
+},
+};
+
 const NavbarSection = () => {
-  return (
-    <div
-      className="black-rectangle"
-      style={{
-        width: "20%",
-        height: "100%",
-        backgroundColor: "#040B14",
-        position: "fixed",
-        left: "0",
-        top: "0",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <div className="chair"
+const [hoveredLink, setHoveredLink] = useState(null);
+
+const handleLinkHover = (link) => {
+  setHoveredLink(link);
+};
+
+return (
+  <div
+    className="black-rectangle"
+    style={{
+      width: "20%",
+      height: "100%",
+      backgroundColor: "#040B14",
+      position: "fixed",
+      left: "0",
+      top: "0",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    }}
+  >
+{/** */}
+<div
+        className="chair"
         style={{
           width: "50%",
           height: "18%",
@@ -34,22 +59,33 @@ const NavbarSection = () => {
           marginTop: "15%",
         }}
       >
-          <a href="https://www.linkedin.com/in/keagan-bogart-786951209" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/keagan-bogart-786951209"
+          target="_blank"
+        >
           <Image
-          src="/chair2.avif"
-          alt="Profile Image"
-          style={{
-            width: "100%",
-            height: "100%",
-            objectFit: "cover",
-          }}
-        /> 
+            src="/chair2.avif"
+            alt="Profile Image"
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+            }}
+          />
         </a>
-
       </div>
 
-      <h2 className="name" style={{ marginTop: "10px", marginBottom: "0", color: "#ccc", fontSize: "110%", paddingBottom: "5%" }}>
-      Keagan Bogart
+      <h2
+        className="name"
+        style={{
+          marginTop: "10px",
+          marginBottom: "0",
+          color: "#ccc",
+          fontSize: "110%",
+          paddingBottom: "5%",
+        }}
+      >
+        Keagan Bogart
       </h2>
 
       <div
@@ -72,12 +108,13 @@ const NavbarSection = () => {
             target="_blank"
             rel="noopener noreferrer"
             style={{
-              color: "#ccc",
-              margin: "0 10px",
-              transition: "color 0.3s ease",
-              textDecoration: "none", // Remove underline
+              ...styles.link,
+              ...(hoveredLink === "linkedin" && styles.hover),
+              ...(hoveredLink && hoveredLink !== "linkedin" && styles.blur),
             }}
             className="icon-link"
+            onMouseEnter={() => handleLinkHover("linkedin")}
+            onMouseLeave={() => handleLinkHover(null)}
           >
             <FaLinkedin size={30} />
           </a>
@@ -86,24 +123,26 @@ const NavbarSection = () => {
             target="_blank"
             rel="noopener noreferrer"
             style={{
-              color: "#ccc",
-              margin: "0 10px",
-              transition: "color 0.3s ease",
-              textDecoration: "none", // Remove underline
+              ...styles.link,
+              ...(hoveredLink === "github" && styles.hover),
+              ...(hoveredLink && hoveredLink !== "github" && styles.blur),
             }}
             className="icon-link"
+            onMouseEnter={() => handleLinkHover("github")}
+            onMouseLeave={() => handleLinkHover(null)}
           >
             <FaGithubSquare size={30} />
           </a>
           <a
             href="mailto:keagan.j.bogart@gmail.com"
             style={{
-              color: "#ccc",
-              margin: "0 10px",
-              transition: "color 0.3s ease",
-              textDecoration: "none", // Remove underline
+              ...styles.link,
+              ...(hoveredLink === "mail" && styles.hover),
+              ...(hoveredLink && hoveredLink !== "mail" && styles.blur),
             }}
             className="icon-link"
+            onMouseEnter={() => handleLinkHover("mail")}
+            onMouseLeave={() => handleLinkHover(null)}
           >
             <RiMailFill size={30} />
           </a>
@@ -117,106 +156,96 @@ const NavbarSection = () => {
             marginBottom: "10px",
           }}
         >
-          <a
-            href="#home"
-            style={{
-              color: "#ccc",
-              margin: "5px 0",
-              transition: "color 0.3s ease",
-              paddingTop: "20%",
-              textDecoration: "none", // Remove underline
-              fontSize: "85%",
-            }}
-            className="section-link"
-          >
-            <AiFillHome /> Home
-          </a>
-          
-          <a
-            href="#about"
-            style={{
-              color: "#ccc",
-              margin: "5px 0",
-              transition: "color 0.3s ease",
-              paddingTop: "10%",
-              textDecoration: "none", // Remove underline
-              fontSize: "85%",
-            }}
-            className="section-link"
-          >
-            <BsPersonFill /> About
-          </a>
-
-          <a
-            href="#experience"
-            style={{
-              color: "#ccc",
-              margin: "5px 0",
-              transition: "color 0.3s ease",
-              paddingTop: "10%",
-              textDecoration: "none", // Remove underline
-              fontSize: "85%",
-
-            }}
-            className="section-link"
-          >
-            <GiTeacher /> Experience
-          </a>
-
-
-          <a
-            href="https://drive.google.com/file/d/1_dPJU4Snvlp7gi9mNe0kWtDGeRKrnCmr/view?usp=drive_link" target="_blank"
-            style={{
-              color: "#ccc",
-              margin: "5px 0",
-              transition: "color 0.3s ease",
-              paddingTop: "10%",
-              textDecoration: "none", // Removes underline
-              fontSize: "85%",
-
-            }}
-            className="section-link"
-          >
-            <IoIosPaper /> Resume
-          </a>
-
-          <a
-            href="mailto:keagan.j.bogart@gmail.com"
-            style={{
-              color: "#ccc",
-              margin: "5px 0",
-              transition: "color 0.3s ease",
-              paddingTop: "10%",
-              textDecoration: "none", // Removes underline
-              fontSize: "85%",
-
-            }}
-            className="section-link"
-          >
-            <GiRotaryPhone /> Contact
-          </a>
-        </div>
-
-        {/* Footer */}
-        <div className="nav-icons"
+        <a
+        href="#home"
         style={{
-            position: "absolute",
-            bottom: "0",
-            // width: "100%",
-            paddingBottom: "8%",
-            color: "#ccc",
-            textAlign: "center",
-            fontSize: "14px",
-            marginBottom: "10px", //margin bottom for spacing
-            
+          ...styles.link,
+          ...(hoveredLink === "home" && styles.hover),
+          ...(hoveredLink && hoveredLink !== "home" && styles.blur),
         }}
-        
-        >
-          {/* Created by Keagan Bogart */}
-        </div>
+        className="section-link"
+        onMouseEnter={() => handleLinkHover("home")}
+        onMouseLeave={() => handleLinkHover(null)}
+      >
+        <AiFillHome /> Home
+      </a>
+       <a
+        href="#about"
+        style={{
+          ...styles.link,
+          ...(hoveredLink === "about" && styles.hover),
+          ...(hoveredLink && hoveredLink !== "about" && styles.blur),
+        }}
+        className="section-link"
+        onMouseEnter={() => handleLinkHover("about")}
+        onMouseLeave={() => handleLinkHover(null)}
+      >
+        <BsPersonFill /> About
+      </a>
+
+      <a
+        href="#experience"
+        style={{
+          ...styles.link,
+          ...(hoveredLink === "experience" && styles.hover),
+          ...(hoveredLink && hoveredLink !== "experience" && styles.blur),
+        }}
+        className="section-link"
+        onMouseEnter={() => handleLinkHover("experience")}
+        onMouseLeave={() => handleLinkHover(null)}
+      >
+        <GiTeacher /> Experience
+      </a>
+
+      <a
+        href="https://drive.google.com/file/d/1_dPJU4Snvlp7gi9mNe0kWtDGeRKrnCmr/view?usp=drive_link"
+        target="_blank"
+        style={{
+          ...styles.link,
+          ...(hoveredLink === "resume" && styles.hover),
+          ...(hoveredLink && hoveredLink !== "resume" && styles.blur),
+        }}
+        className="section-link"
+        onMouseEnter={() => handleLinkHover("resume")}
+        onMouseLeave={() => handleLinkHover(null)}
+      >
+        <IoIosPaper /> Resume
+      </a>
+
+      <a
+        href="mailto:keagan.j.bogart@gmail.com"
+        style={{
+          ...styles.link,
+          ...(hoveredLink === "contact" && styles.hover),
+          ...(hoveredLink && hoveredLink !== "contact" && styles.blur),
+        }}
+        className="section-link"
+        onMouseEnter={() => handleLinkHover("contact")}
+        onMouseLeave={() => handleLinkHover(null)}
+      >
+        <GiRotaryPhone /> Contact
+      </a>
+      </div>
+      </div>
+
+            {/* Footer */}
+            <div
+              className="nav-icons"
+              style={{
+                position: "absolute",
+                bottom: "0",
+                paddingBottom: "8%",
+                color: "#ccc",
+                textAlign: "center",
+                fontSize: "14px",
+                marginBottom: "10px",
+              }}
+            >
+        {/* Created by Keagan Bogart */}
       </div>
     </div>
   );
+
 };
 
 export default NavbarSection;
