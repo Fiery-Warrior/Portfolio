1f2f720033fc80992cd8e758e64e0cc93dd63c55
Keagan Bogart <keagan.j.bogart@gmail.com>
2023-12-04 14:41:41 -0600

    Added Theorem area

--- src/Isa/Isa.jsx
+++ src/Isa/Isa.jsx
@@ -1,143 +1,10 @@
-// import React, { useState, useEffect } from "react";
-// import "./ed.css";
-// import Typed from "react-typed";
-// import Typography from '@mui/material/Typography';
-// import RowColumnPosition from "./Typing/Type.jsx";
-// import RowNumber from "./Typing/RowNumber.jsx"
-// import ColumnNumber from "./Typing/ColumnNumber.jsx"
-
-// const Isa = () => {
-//   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-//   const [isTypedComplete, setIsTypedComplete] = useState(false);
-
-//   const handleTypedComplete = () => {
-//     setIsTypedComplete(true);
-//   };
-
-//   useEffect(() => {
-//     const handleResize = () => {
-//       setScreenWidth(window.innerWidth);
-//     };
-
-//     window.addEventListener("resize", handleResize);
-
-//     return () => {
-//       window.removeEventListener("resize", handleResize);
-//     };
-//   }, []);
-
-//   let headerText = "Independent Set Algorithm";
-//   if (screenWidth < 499) {
-//     headerText = "Education";
-//   }
-
-//   const sectionStyle = {
-//     color: "#000000",
-//     backgroundColor: "rgb(216 219 223)",
-//     width: "170vh",
-//     display: "flex",
-//     alignItems: "center",
-//     justifyContent: "center",
-//     boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-//     height: "200vh",
-//     paddingLeft: "20%", /**Moves not just the heade 'Recent Experience' but also the cards */
-//     marginTop: '0',
-//   };
-
-
-
-//   const awardStyle = {
-//     marginBottom: "10px", // Adds margin-bottom to create space
-//   };
-
-//   return (
-//     <section id="education" style={sectionStyle} className="education-section">
-//       <div className="ed-container">
-//         {/* <div className="ed-header">{headerText}</div> */}
-//         <div className="ed-header">      
-//         <h1 style={{color: "#252525"}}>How to use the {" "}
-//         {!isTypedComplete ? (
-//             <Typed
-//               strings={["Bipartite Matrix Calculator"]}
-//               typeSpeed={120}
-//               backSpeed={140}
-//               loop={false}
-//               style={{
-//                 color: "grey",
-//                 borderBottom: "3px solid #149ddd",
-//               }}
-//               onComplete={handleTypedComplete}
-//             />
-//           ) : (
-//             <span style={{ color: "grey", borderBottom: "3px solid #149ddd",}}>Bipartite Matrix Calculator</span>
-//           )}
-// </h1></div>
-//         <div className="content">
-//           <section className="ed-awards">
-//             <div style={awardStyle}>
-//             </div>
-//             {/* <Awards /> */}
-//           </section>
-//           <div className="ed-bachelors">
-//             <Typography gutterBottom variant="h5" component="div" sx={{ display: 'grid', gridTemplateColumns: 'repeat(1, 1fr)', gap: '30px', '@media (max-width: 550px)': { fontSize: '13px', gap: '5px' } }}>
-//               <span>‣ Enter Number of Rows</span>
-//               <span>‣ Enter Number of Columns</span>
-//               <span>‣ Enter Row/Column data (0 or 1)</span>
-//               <span>‣ View Calculated:
-//                   <Typography gutterBottom variant="p" component="div" sx={{ display: 'grid', gridTemplateColumns: 'repeat(1, 1fr)', padding: '15px',fontSize: '20px', gap: '10px', '@media (max-width: 550px)': { fontSize: '8px', gap: '3px' } }}>
-//                     <span>‣ Max Matchings</span>
-//                     <span>‣ Min Coverings</span>
-//                     <span>‣ Updated ISA Matrix</span>
-//                 </Typography>
-//               </span>
-    
-//             </Typography>
-//           </div>
-
-//           <div className='row-number'>
-//             <RowNumber/>
-//           </div>
-
-//           <div className='column-number'>
-//             <ColumnNumber/>
-//           </div>
-
-//           <div className='row-column-position'>
-//           <RowColumnPosition />
-//           </div>
-
-
-
-
-//           <section className="languages">
-//             <div>
-//               <video width="620" height="440" controls>
-//                 <source src="/Speed_Matrix.mp4" type="video/mp4" />
-//                 Your browser does not support the video tag.
-//               </video>
-//             </div>
- 
-//             {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/jZiZs8cZAKU?si=n-XcMhcay-rvQ-dK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
-
-//           </section>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Isa;
-
-
 import React, { useState, useEffect, useRef } from "react";
 import "./ed.css";
 import Typed from "react-typed";
 import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
 import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
-import RowColumnPosition from "./Typing/Type.jsx";
-import RowNumber from "./Typing/RowNumber.jsx"
-import ColumnNumber from "./Typing/ColumnNumber.jsx"
+import Theorem from "./Theorem";
 
 
 const Isa = () => {
@@ -145,9 +12,15 @@ const Isa = () => {
   const [isTypedComplete, setIsTypedComplete] = useState(false);
   const typedRef = useRef(null);
   const contentRef = useRef(null);
+  const [showBorder, setShowBorder] = useState(false);
+
+
+  
+
 
   const handleTypedComplete = () => {
     setIsTypedComplete(true);
+    setShowBorder(true); // Set showBorder to true after typing completion
   };
 
   useEffect(() => {
@@ -254,7 +127,10 @@ const Isa = () => {
             />
           ) : (
             <span style={{ color: "grey", borderBottom: "3px solid #149ddd",}}>Bipartite Matrix Calculator:</span>
-          )}</h1></div>
+          )}
+          {showBorder && ( // Conditionally render the border underline
+          <span style={{ borderBottom: "3px solid #149ddd" }} />
+        )}</h1></div>
         <div className="content">
           <section className="ed-awards">
             <div style={awardStyle}>
@@ -273,11 +149,25 @@ const Isa = () => {
               arrow // Enable arrow for the tooltip
             >
         <span>‣ Enter <span style={{borderBottom: '3px solid rgb(20, 157, 221)'}}>Number of Rows</span></span>
+
+        {/* <span>
+        ‣ Enter{" "}
+        {!isTypedComplete ? (
+          <span style={{ borderBottom: '3px solid transparent' }}>
+            Number of Rows
+          </span>
+        ) : (
+          <span style={{ borderBottom: showBorder ? '3px solid rgb(20, 157, 221)' : 'none' }}>
+            Number of Rows
+          </span>
+        )}
+      </span> */}
+
+
+
+
+
       </HtmlTooltip>
-
-
-
-
               {/* <span>‣ Enter Number of Columns</span> */}
         <HtmlTooltip
               title={
@@ -321,30 +211,15 @@ const Isa = () => {
                     <span>‣ Updated ISA Matrix</span>
                 </Typography>
               </span>
-    
             </Typography>
-
-
           </div>
-
-          {/* <div className='row-number'>
-            <RowNumber/>
-          </div>
-
-          <div className='column-number'>
-            <ColumnNumber/>
-          </div>
-
-          <div className='row-column-position'>
-          <RowColumnPosition />
-          </div> */}
-
           <section className="languages">
             <div>
               <video width="620" height="440" controls>
                 <source src="/Speed_Matrix.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
+              <Theorem/>
             </div>
  
             {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/jZiZs8cZAKU?si=n-XcMhcay-rvQ-dK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
---
+++ src/Isa/Theorem.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./theorem.css";
+
+
+
+function Theorem() {
+
+  return (
+    <>
+        <a class="fancy" href="#">
+        <span class="top-key"></span>
+        <span class="text">Theorems & Rules</span>
+        <span class="bottom-key-1"></span>
+        <span class="bottom-key-2"></span>
+        </a>
+    </>
+  );
+}
+
+
+
+export default Theorem;
